0bc1db23b32130c4fa774083e8cc86a531eb6fb6
Marc-Antoine Malépart <mantoine.malepart@gmail.com>
2025-12-01 17:02:41 -0500

    Solve day 1 part 2

--- Sources/Day1/Day1.swift
+++ Sources/Day1/Day1.swift
@@ -30,6 +30,10 @@ struct Day1: DayCommand {
         printTitle("Part 1", level: .title1)
         let password = part1(moves: moves)
         print("Password:", password, terminator: "\n\n")
+        
+        printTitle("Part 2", level: .title1)
+        let correctPassword = part2(moves: moves)
+        print("Password using method 0x434C49434B:", correctPassword)
     }
     
     private func moves(from lines: [String]) -> [Move] {
@@ -37,20 +41,51 @@ struct Day1: DayCommand {
     }
     
     private func part1(moves: [Move]) -> Int {
-        var position = 50
+        var currentPosition = 50
         var result = 0
         
         for move in moves {
-            position += move.delta
-            
-            if position < 0 {
-                position += 100
+            let rotated = currentPosition + move.delta
+            let (quotient, remainder) = rotated.quotientAndRemainder(dividingBy: 100)
+            let newPosition = if remainder < 0 {
+                (remainder + 100) % 100
             }
-            position %= 100
+            else {
+                remainder
+            }
             
-            if position == 0 {
+            if newPosition == 0 {
                 result += 1
             }
+            
+            currentPosition = newPosition
+        }
+        
+        return result
+    }
+    
+    private func part2(moves: [Move]) -> Int {
+        var currentPosition = 50
+        var result = 0
+        
+        for move in moves {
+            let rotated = currentPosition + move.delta
+            let (quotient, remainder) = rotated.quotientAndRemainder(dividingBy: 100)
+            let newPosition = if remainder < 0 {
+                (remainder + 100) % 100
+            }
+            else {
+                remainder
+            }
+            
+            var rotations = abs(quotient)
+            if currentPosition != 0, case .left = move, rotated <= 0 {
+                // If we didn't start from zero and rotated left to zero or wrapped around, we count an extra rotation.
+                rotations += 1
+            }
+            
+            result += rotations
+            currentPosition = newPosition
         }
         
         return result
