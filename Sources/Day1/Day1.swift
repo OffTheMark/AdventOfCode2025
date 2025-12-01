2142a1cbde31777a385c8d210a8e4eefac7eb160
Marc-Antoine Malépart <mantoine.malepart@gmail.com>
2025-12-01 08:17:23 -0500

    Solve day 1 part 1

--- Sources/Day1/Day1.swift
+++ Sources/Day1/Day1.swift
@@ -8,6 +8,7 @@
 import Foundation
 import ArgumentParser
 import AdventOfCodeUtilities
+import RegexBuilder
 
 struct Day1: DayCommand {
     static var configuration: CommandConfiguration {
@@ -24,9 +25,97 @@ struct Day1: DayCommand {
     var puzzleInputURL: URL
     
     func run() throws {
-        let file = try readFile()
+        let moves = moves(from: try readLines())
         
         printTitle("Part 1", level: .title1)
-        // TODO: Solve part 1
+        let password = part1(moves: moves)
+        print("Password:", password, terminator: "\n\n")
+    }
+    
+    private func moves(from lines: [String]) -> [Move] {
+        lines.compactMap(Move.init)
+    }
+    
+    private func part1(moves: [Move]) -> Int {
+        var position = 50
+        var result = 0
+        
+        for move in moves {
+            position += move.delta
+            
+            if position < 0 {
+                position += 100
+            }
+            position %= 100
+            
+            if position == 0 {
+                result += 1
+            }
+        }
+        
+        return result
     }
 }
+
+private enum Move: LosslessStringConvertible {
+    case left(Int)
+    case right(Int)
+    
+    var delta: Int {
+        switch self {
+        case .left(let count):
+            -count
+            
+        case .right(let count):
+            count
+        }
+    }
+    
+    init?(_ description: String) {
+        let regex = Regex {
+            Capture {
+                ChoiceOf {
+                    "L"
+                    "R"
+                }
+            }
+            
+            TryCapture {
+                Repeat(0...) {
+                    CharacterClass.digit
+                }
+                
+                CharacterClass.digit
+            } transform: {
+                Int($0)
+            }
+        }
+        .anchorsMatchLineEndings()
+        
+        guard let (_, direction, count) = description.wholeMatch(of: regex)?.output else {
+            return nil
+        }
+        
+        switch direction {
+        case "L":
+            self = .left(count)
+            
+        case "R":
+            self = .right(count)
+            
+        default:
+            return nil
+        }
+    }
+    
+    var description: String {
+        switch self {
+        case .left(let count):
+            "L\(count)"
+            
+        case .right(let count):
+            "R\(count)"
+        }
+    }
+}
+
